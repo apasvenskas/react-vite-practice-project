4f0356d6342e3bdb9109707450e6be42050371a8
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-08-29 18:33:57 -0700

    added a new file called newpost and adjusted css acordingly

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,30 @@
-import Post from "./Post"
-import classes from './PostList.module.css'; 
+import Post from "./Post";
+import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
+import { useState } from "react";
 
 export default function PostList() {
-    return (
-        <ul className={classes.posts}>
-         <Post author="Audrius" body="Another React practice project!"/>
-         <Post author="Andrew" body="React is great! It is my favorite front end librery!"/>
-    </ul>
-    )
-};
+  const [textBody, setTextBody] = useState('');   
+  const [enteredName, setEnteredName] = useState(''); 
+
+  function textBodyChangeHandler(event){
+    setTextBody(event.target.value);
+  }
+
+  function nameChangeHandler(event){
+    setEnteredName(event.target.value);
+  }
+
+  return (
+    <>
+    <NewPost onTextBody={textBodyChangeHandler} onNameChange={nameChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredName} body={textBody} />
+        <Post
+          author="Andrew"
+          body="React is great! It is my favorite front end librery!"
+        />
+      </ul>
+    </>
+  );
+}
